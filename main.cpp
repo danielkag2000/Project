202c0115621acb2390f757f7312d1c98a2868462
evyatar18 <evyatar38@gmail.com>
2018-12-17 18:03:46 +0200

    exceptions.h

--- main.cpp
+++ main.cpp
@@ -8,5 +8,9 @@ int main() {
     SymbolTable table;
     table.set("hello", 4.3);
     table.bind("hello", "fuck/you");
+    table.set("hello", 4.5);
+
+    cout << table.get("hello") << endl;
+    cout << sizeof(string) << endl;
     return 0;
 }
--- vars/stable.h
+++ vars/stable.h
@@ -11,16 +11,41 @@ private:
     double _val;
     string _handle;
 
-
-
     void saveData();
-    void retrieveData();
 public:
-    double getValue();
-    void setValue(double val);
+    /**
+     * Get value of this variable
+     * @return the value
+     */
+    double get();
 
+    /**
+     * Update the inner value of this variable without sending to server.
+     * @param val the new value
+     */
+    void update(double val);
+
+    /**
+     * Set the inner value of this variable, and send to server.
+     * @param val the new value
+     */
+    void set(double val);
+
+    /**
+     * Bind this variable to a given handle.
+     * @param handle
+     */
     void bind(const string& handle);
+
+    /**
+     * Unbind this variable.
+     */
     void unbind();
+
+    /**
+     * Query whether this variable is bound.
+     * @return true if it is.
+     */
     bool bound();
 };
 
@@ -28,13 +53,13 @@ class SymbolTable {
 private:
     unordered_map<string, Variable> _vars;
 public:
-    bool exists(const string& name);
-
-    void set(const string& name, double val);
-    double get(const string& name);
-
     void bind(const string& name, const string& handle);
     void unbind(const string& name);
+
+    void update(const string& var, double newVal);
+    void set(const string& var, double val);
+
+    double get(const string& name);
 };
 
 
