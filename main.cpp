60e0e3a6704025b080705e1cf6809e7058a1f681
evyatar18 <evyatar38@gmail.com>
2018-12-17 15:53:23 +0200

    fuck u

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "Hi there! are u a loli-lover? ;D" << std::endl;
+    cout << "Hello, World!" << endl;
+    cout << "Hi there! are u a loli-lover? ;D" << endl;
+
+    cout << "8===D" << endl;
     return 0;
 }
