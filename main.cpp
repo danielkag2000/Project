7fb05a49c1890acf243ddba85d40375ca318ba54
evyatar18 <evyatar38@gmail.com>
2018-12-18 21:03:45 +0200

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 #include "vars/stable.h"
 #include "algorithms/lexer.h"
+#include "algorithms/shunting_yard_algorithm.h"
 
 using namespace std;
 
@@ -21,9 +22,13 @@ int main() {
 
     vector<string> v = lexer(str);
 
-    for(string &s : v) {
-        cout << s << endl;
+    operators ops{ { "+", 1 }, { "-", 1 }, { "*", 2 }, { "/", 2 } };
+    vector<string> postfix = run_shunting_yard(ops, v);
+
+    for (string& s : postfix) {
+        cout << s << "";
     }
+    cout << endl;
 
     return 0;
 }
